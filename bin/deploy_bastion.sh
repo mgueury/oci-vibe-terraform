6745e7343c3d667b84abb05badf04936da27c399
Marc Gueury <marc.gueury@oracle.com>
2026-05-08 15:35:18 +0200

    v3.0

--- bin/deploy_bastion.sh
+++ bin/deploy_bastion.sh
@@ -34,7 +34,7 @@ function setup_bastion_dir() {
     fi
     cp $TARGET_DIR/tf_env.sh $BASTION_DIR/compute/.
 
-    if [ "$TF_VAR_deploy_type" == "public_compute" ] then
+    if [ "$TF_VAR_deploy_type" == "public_compute" ]; then
         if -d src/compute; then 
             cp -R src/compute/* $BASTION_DIR/.
         fi
