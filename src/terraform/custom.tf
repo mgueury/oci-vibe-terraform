b8cef45a73bead51a693a062432fb9381aabb089
Marc Gueury <marc.gueury@oracle.com>
2026-05-09 19:00:50 +0200

    v3.0

--- src/terraform/custom.tf
+++ src/terraform/custom.tf
@@ -8,22 +8,22 @@ variable genai_model {
 }
 
 resource "null_resource" "custom_dependency" {
-  provisioner "local-exec" {
-    command = <<-EOT
-    cd ${local.project_dir}
-    ENV_FILE=target/tf_env.sh
-    append() {
-      echo "$1" >> $ENV_FILE
-    }    
-    append_export() {
-      if [ "$2" != "" ] && [ "$2" != "-" ]; then
-        echo "export $1=\"$2\"" >> $ENV_FILE
-      fi 
+    provisioner "local-exec" {
+        command = <<-EOT
+        cd ${local.project_dir}
+        ENV_FILE=target/tf_env.sh
+        append() {
+            echo "$1" >> $ENV_FILE
+        }    
+        append_export() {
+            if [ "$2" != "" ] && [ "$2" != "-" ]; then
+                echo "export $1=\"$2\"" >> $ENV_FILE
+            fi 
+        }
+        append "# Custom"
+        append_export "TF_VAR_genai_api_key" "${local.genai_api_key}"
+        append_export "TF_VAR_genai_model" "${local.genai_model}"
+        EOT
     }
-    append "# Custom"
-    append_export "TF_VAR_genai_api_key" "${local.genai_api_key}"
-    append_export "TF_VAR_genai_model" "${local.genai_model}"
-    EOT
-
     depends_on = [ null_resource.tf_env ]
 }
